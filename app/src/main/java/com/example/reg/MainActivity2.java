aa20e052ef8b0aec986e1d3086194b8ea3c11975
schanchal2107 <chanchal.sharma@cs.christuniversity.in>
2021-04-04 17:21:53 +0530

    Initial commit

---
+++ app/src/main/java/com/example/reg/MainActivity2.java
@@ -0,0 +1,91 @@
+package com.example.reg;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import org.w3c.dom.Text;
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Calendar;
+public class MainActivity2 extends Activity {
+    EditText date;
+    DatePickerDialog datePickerDialog;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        date = (EditText) findViewById(R.id.date);
+        // perform click event on edit text
+        date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // calender class's instance and get current date , month and year from calender
+                final Calendar c = Calendar.getInstance();
+                int mYear = c.get(Calendar.YEAR); // current year
+                int mMonth = c.get(Calendar.MONTH); // current month
+                int mDay = c.get(Calendar.DAY_OF_MONTH); // current day
+                // date picker dialog
+                datePickerDialog = new DatePickerDialog(MainActivity2.this,
+                        new DatePickerDialog.OnDateSetListener() {
+
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int monthOfYear, int dayOfMonth) {
+                                // set day of month , month and year value in the edit text
+                                date.setText(dayOfMonth + "/"
+                                        + (monthOfYear + 1) + "/" + year);
+
+                            }
+                        }, mYear, mMonth, mDay);
+                datePickerDialog.show();
+            }
+        });
+
+        Intent intent=getIntent();
+        TextView name =(TextView)findViewById(R.id.name);
+        name.setText(intent.getStringExtra("name"));
+        TextView email =(TextView)findViewById(R.id.email);
+        email.setText(intent.getStringExtra("email"));
+        TextView age =(TextView)findViewById(R.id.age);
+        age.setText(intent.getStringExtra("age"));
+        TextView gender=(TextView)findViewById(R.id.gender);
+        gender.setText(intent.getStringExtra("radioSex"));
+        TextView branch=(TextView)findViewById(R.id.branch);
+        branch.setText(intent.getStringExtra("branch"));
+        TextView language=(TextView)findViewById(R.id.language);
+        language.setText(intent.getStringExtra("language"));
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main2, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void Submit(View view) {
+        Toast.makeText(MainActivity2.this, "Receipt submitted!! Kindly come on selected date.", Toast.LENGTH_LONG).show();
+
+    }
+}
